3b2ae3609e4e0831972219a87c8d6ab3308af81e
Ratsock6 <ratsockfofox@gmail.com>
2025-08-03 15:41:44 +0200

    realloc

---
+++ realloc.c
@@ -0,0 +1,27 @@
+#include "malloc.h"
+#include <string.h>
+#include <unistd.h>
+
+void *realloc(void *ptr, size_t size) {
+    if (!ptr)
+        return ft_malloc(size);
+
+    if (size == 0)
+    {
+        free(ptr);
+        return NULL;
+    }
+
+    t_block *block = (t_block *)ptr - 1;
+    if (block->size >= size)
+        return ptr;
+
+    void *new_ptr = malloc(size);
+    if (!new_ptr)
+        return NULL;
+
+    memcpy(new_ptr, ptr, block->size);
+
+    free(ptr);
+    return new_ptr;
+}
